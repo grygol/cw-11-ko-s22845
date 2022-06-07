f703ab9957b8dbffca6f85cdce91a411422443ea
michal <s22845@pjwstk.edu.pl>
2022-06-07 14:46:20 +0200

    Init commit

--- APBD11/Startup.cs
+++ APBD11/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using APBD11.Data;
+using APBD11.Services;
 
 namespace APBD11
 {
@@ -26,6 +27,7 @@ namespace APBD11
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IStudentService, StudentService>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
