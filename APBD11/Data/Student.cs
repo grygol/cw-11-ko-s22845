32350b5100fbd2ba7eda6dec12f58f0f67a209d0
michal <s22845@pjwstk.edu.pl>
2022-06-07 17:29:16 +0200

    Basic logic

---
+++ APBD11/Data/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace APBD11.Data
+{
+	public class Student
+	{
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Studies { get; set; }
+        public string AvatarUrl { get; set; }
+
+
+        public Student()
+		{
+            AvatarUrl = "https://cdn4.iconfinder.com/data/icons/professions-1-2/151/8-512.png";
+
+        }
+	}
+}
+
